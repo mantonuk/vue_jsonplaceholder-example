1c9ce0285b15ec772fb8c80c02db85cd3e076c51
Maxim Antonuk <maxim.antonuk@gmail.com>
2020-11-22 19:26:12 +0200

    Posts

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
+    <PageHeader somePropsValue="somePropsValue Header"/>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <Posts title="Posts" />
+    <PageFooter somePropsValue="somePropsValue Footer"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PageHeader from './components/PageHeader.vue'
+import Posts from './components/Posts.vue'
+import PageFooter from './components/PageFooter.vue'
+/*import HelloWorld from './components/HelloWorld.vue'*/
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PageHeader,
+    Posts,
+    PageFooter,
+    /*HelloWorld*/
   }
 }
 </script>
